65694e9aefd5709bcc07a5c0c406cea9d1d6c4c5
Olya7me <gumenyucko@yandex.ru>
2024-10-04 13:39:36 +0500

    fixed link

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 //Сюда импортируйте свои модули из папаки modules и вызывайте функции
 
 import { loadSidebar } from './modules/sidebar';
-import { setupSearchRedirect } from './modules/search';
 
 document.addEventListener('DOMContentLoaded', () => {
     loadSidebar();
-    setupSearchRedirect();
 });
