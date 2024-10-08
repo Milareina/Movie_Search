9010fa224deda25ed56fd9b723dcb61f4fdf9c2d
Olesia <kujavia@yandex.ru>
2024-10-08 17:14:16 +0300

    first commit series

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,34 @@
-import { loadSidebar } from './modules/sidebar';
-import { getRecommendMovies, initEventListeners } from './modules/search';
+import { loadSidebar } from "./modules/sidebar";
+import { getRecommendMovies, initEventListeners } from "./modules/search";
+import {
+  movieListContainer,
+  fetchSeries,
+  displaySeries,
+  updateItemsPerView,
+  showNextItems,
+} from "./modules/series";
 
-document.addEventListener('DOMContentLoaded', () => {
-    loadSidebar();
-    initEventListeners();
-    getRecommendMovies("https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_ALL&page=1");
+document.addEventListener("DOMContentLoaded", () => {
+  loadSidebar();
+  initEventListeners();
+  getRecommendMovies(
+    "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_ALL&page=1"
+  );
+  //   series
+  movieListContainer,
+    fetchSeries(),
+    displaySeries(),
+    updateItemsPerView(),
+    showNextItems();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchSeries();
+  const arrow = document.getElementById("arrow");
+  arrow.addEventListener("click", showNextItems);
+
+  window.addEventListener("resize", () => {
+    updateItemsPerView();
+    showNextItems();
+  });
 });
